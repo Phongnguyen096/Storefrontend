7c508dfb38bb6a81d00fa0f675168f75a41526ee
Phong Nguyen <nguyendacphong096@gmail.com>
2023-07-13 18:26:40 +0700

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,13 @@
-import { IconButton } from "@mui/material";
+import {  Button, ThemeProvider, Typography} from "@mui/material";
 import "./Header.scss"
-import { AddShoppingCart } from "@mui/icons-material";
+import {styles} from "./theme";
 function Header() {
-    return ( <div className="Header">
-        <div className="logo-store">
-            <div>PG</div>
-        </div>
-        <div className="slogan"><div>MUSIC FOR LIFE</div></div>
-        <div className="search-bar">
-           <div className="search-icon"></div>
-           <div className="search-input"></div>
-        </div>
-        <div className="deader">
-           <div className="deader-icon"></div>
-           <div className="deader-description"></div>
-        </div>
-        <div className="my-account">
-           <div className="my-account-icon"></div>
-           <div className="my-account-description"></div>
-        </div>
-        <div className="cart">
-            <IconButton  aria-label="add to shopping cart" href="products">
-                <AddShoppingCart sx={{color:"white"}}/>
-            </IconButton>
-        </div>
-    </div> );
+        const  classes = styles() ;
+        return ( <div className="Header">
+        <Typography className={classes.btn}>LOGO</Typography>
+        <Typography>Slogan</Typography>    
+             <Button color="primary">Login</Button>
+        </div> );
 }
 
 export default Header;
